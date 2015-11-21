8fde5e6ef34304832a09c2382423819af41941ea
Ruman Gerst <ruman.gerst@uni-jena.de>
2015-11-21 01:27:18 +0100

    * philosophers

--- u4/philosophers/main.cpp
+++ u4/philosophers/main.cpp
@@ -35,13 +35,10 @@ int main(int argc, char * argv[])
     {
         cout << "philosophers <runtime s>" << endl;
         exit(EXIT_FAILURE);
-    }
-    
-    omp_set_num_threads(N + 1); // 1 master + N philosophers
+    }    
 
     // Monitor the time
-    int time_limit __attribute__((aligned(MEMORY_ALIGNMENT))) = stoi(argv[1]);
-    bool time_up __attribute__((aligned(MEMORY_ALIGNMENT))) = false;
+    const int time_limit __attribute__((aligned(MEMORY_ALIGNMENT))) = stoi(argv[1]);
 
     // There are N philosophers at the table
     // They are discussing
@@ -56,29 +53,8 @@ int main(int argc, char * argv[])
 
     // Monitor the time
     const high_resolution_clock::time_point time_start = high_resolution_clock::now();
-
-    //Create a time watcher thread
-
-    #pragma omp parallel shared(time_up)
-    {
-
-    #pragma omp master
-    while(!time_up)
-    {
-        const double dur = duration<double>(high_resolution_clock::now()-time_start).count();
-
-	if(dur >= time_limit)
-	{
-            //#pragma omp atomic write
-            // Dont know why but leads to effect that this variable is not set!
-            time_up = true;
-        }       
-        
-    }
-
-    //cout << "starting philosophers"<<endl;
-
-    #pragma omp for schedule(static, 1)
+   
+    #pragma omp parallel for schedule(static, 1) firstprivate(time_start,time_limit) shared(discussions,table) default(none)
     for(int i = 0; i < N; ++i)
     {
         //cout << "Philososopher "<< i << " comes alive" << endl;
@@ -89,20 +65,24 @@ int main(int argc, char * argv[])
         // 1 object per thread prevents need of locks
         default_random_engine random_generator __attribute__((aligned(MEMORY_ALIGNMENT)));
         uniform_int_distribution<int> philosopher_time(DISCUSSION_EATING_TIME);
+        
+        bool _time_up = false;
 
-        while(!time_up)
+        while(!_time_up)
         {        
             #pragma omp atomic update
             ++discussions;
 
             //this_thread::sleep_for(microseconds(philosopher_time(random_generator)));
-            //boost::this_thread::sleep_for(boost::chrono::microseconds(philosopher_time(random_generator)));
             usleep(philosopher_time(random_generator));
 
             bool can_eat = false;
 
-            while(!can_eat && !time_up)
+            while(!can_eat && !_time_up)
             {
+            	const double dur = duration<double>(high_resolution_clock::now()-time_start).count();
+            	_time_up = dur >= time_limit;
+            
                 bool x_left;
                 bool x_right;
                 
@@ -141,7 +121,6 @@ int main(int argc, char * argv[])
 
             // The philosopher is eating
             //this_thread::sleep_for(microseconds(philosopher_time(random_generator)));
-            //boost::this_thread::sleep_for(boost::chrono::microseconds(philosopher_time(random_generator)));
             usleep(philosopher_time(random_generator));
 
             //Put the forks back
@@ -155,8 +134,7 @@ int main(int argc, char * argv[])
         }      
         
         //cout << "philosopher " << i << "ended."  << endl;
-    }
-    }
+    }    
 
     auto elapsed_seconds = duration<double>(high_resolution_clock::now() - time_start).count();
     cout << "Runtime: " << elapsed_seconds << "s" << endl;
